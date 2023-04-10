018faf5416dd9a0edcee7409f5ed82e0b898f6d2
AnushaManik <93906943+AnushaManik@users.noreply.github.com>
2023-04-10 13:51:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+var speechRecognition = window.webkitSpeechRecognition;
+var recognition = new speechRecognition();
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+    console.log("startclick")
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var content = event.results[0][0].transcript
+    console.log(content);
+    document.getElementById("textbox").innerHTML = content;
+     if(content == "take my selfie"){
+        console.log("taking selfie in 5 seconds")
+        speak();
+     }
+    
+}
+function speak(){
+    var synth = window.speechSynthesis 
+    speakdata = "taking selfie in 5 seconds"
+    var utterthis = new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utterthis);
+}
+Webcam.set({
+    width:320,
+    height:320,
+    image_format:'png',
+    png_quality:50
+
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML = "<img id='selfie_img'src='"+data_uri+"'>"
+
+    })
+
+
+    
+}
+setTimeout(
+   function(){
+    takesnapshot();
+    save();
+   }
+
+,5000);
+function save(){
+link = document.getElementById("link");
+img = document.getElementById("selfie_img").src;
+
+}
+
+
